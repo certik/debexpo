f4b1cbf9e1aa75759af351689d34e7048569d5bc
Jonny Lamb <jonnylamb@jonnylamb.com>
2008-06-03 14:50:19 +0100

    Added debexpo.lib.changes module to abstract reading changes files.
    Signed-off-by: Jonny Lamb <jonnylamb@jonnylamb.com>

---
+++ debexpo/lib/changes.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+#
+#   changes.py — .changes file handling class
+#
+#   This file is part of debexpo - http://debexpo.workaround.org
+#
+#   Copyright © 2008 Jonny Lamb <jonnylamb@jonnylamb.com
+#
+#   Permission is hereby granted, free of charge, to any person
+#   obtaining a copy of this software and associated documentation
+#   files (the "Software"), to deal in the Software without
+#   restriction, including without limitation the rights to use,
+#   copy, modify, merge, publish, distribute, sublicense, and/or sell
+#   copies of the Software, and to permit persons to whom the
+#   Software is furnished to do so, subject to the following
+#   conditions:
+#
+#   The above copyright notice and this permission notice shall be
+#   included in all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+#   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+#   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+#   OTHER DEALINGS IN THE SOFTWARE.
+
+__author__ = 'Jonny Lamb'
+__copyright__ = 'Copyright © 2008 Jonny Lamb'
+__license__ = 'MIT'
+
+from debian_bundle import deb822
+
+class Changes(object):
+    def __init__(self, filename=None, string=None):
+        if (filename and string) or (not filename and not string):
+            raise TypeError
+
+        if filename:
+            self.data = deb822.Changes(file(filename))
+        else:
+            self.data = deb822.Changes(string)
+
+        self.source = self.data['Source']
+
+    def get_files(self):
+        return [z['name'] for z in self.data['Files']]
