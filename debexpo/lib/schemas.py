094cb44ba981aad3b6ddd390039f080c656837e0
Jonny Lamb <jonnylamb@jonnylamb.com>
2008-06-24 14:31:18 +0100

    Added schemas and validators to debexpo.lib.schemas and debexpo.lib.validators respectively.
    Signed-off-by: Jonny Lamb <jonnylamb@jonnylamb.com>

---
+++ debexpo/lib/schemas.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+#
+#   schemas.py — Form schemas
+#
+#   This file is part of debexpo - http://debexpo.workaround.org
+#
+#   Copyright © 2008 Jonny Lamb <jonnylamb@jonnylamb.com
+#
+#   Permission is hereby granted, free of charge, to any person
+#   obtaining a copy of this software and associated documentation
+#   files (the "Software"), to deal in the Software without
+#   restriction, including without limitation the rights to use,
+#   copy, modify, merge, publish, distribute, sublicense, and/or sell
+#   copies of the Software, and to permit persons to whom the
+#   Software is furnished to do so, subject to the following
+#   conditions:
+#
+#   The above copyright notice and this permission notice shall be
+#   included in all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+#   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+#   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+#   OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+Holds form schemas for debexpo forms.
+"""
+
+__author__ = 'Jonny Lamb'
+__copyright__ = 'Copyright © 2008 Jonny Lamb'
+__license__ = 'MIT'
+
+import formencode
+
+from pylons import session
+
+from debexpo.lib.validators import NewEmailToSystem, NewDebianEmailToSystem, GpgKey, \
+    CurrentPassword, CheckBox
+
+class LoginForm(formencode.Schema):
+    """
+    Schema for the login form in the login controller.
+    """
+    email = formencode.validators.Email(not_empty=True)
+    password = formencode.validators.String(not_empty=True)
+    commit = formencode.validators.String()
+
+
+class MyForm(formencode.Schema):
+    """
+    General schema for all forms in the my controller to extend.
+    """
+    commit = formencode.validators.String()
+    form = formencode.validators.String()
+
+class DetailsForm(MyForm):
+    """
+    Schema for updating user details in the my controller.
+    """
+    name = formencode.validators.String(not_empty=True)
+    email = NewEmailToSystem(not_empty=True, allow=session.get('user_id', None))
+
+class GpgForm(MyForm):
+    """
+    Schema for updating the user's GPG key in the my controller.
+    """
+    gpg = GpgKey()
+    delete_gpg = formencode.validators.Int()
+
+class PasswordForm(MyForm):
+    """
+    Schema for updating the user's password in the my controller.
+    """
+    password_current = CurrentPassword()
+    password_confirm = formencode.validators.String(min=6)
+    password_new = formencode.validators.String(min=6)
+
+    # Make sure password_new and password_confirm are the same.
+    chained_validators = [
+        formencode.validators.FieldsMatch('password_new', 'password_confirm')
+    ]
+
+class OtherDetailsForm(MyForm):
+    """
+    Schema for updating other details: country, jabber, ircnick and status
+    in the my controller.
+    """
+    country = formencode.validators.Number()
+    ircnick = formencode.validators.String()
+    jabber = formencode.validators.String()
+    status = CheckBox()
+
+class RegisterForm(formencode.Schema):
+    """
+    Schema for the general fields in the register controller. The maintainer
+    and sponsor forms should extend this.
+    """
+    name = formencode.validators.String(not_empty=True)
+    password = formencode.validators.String(min=6)
+    password_confirm = formencode.validators.String(min=6)
+    commit = formencode.validators.String()
+
+    # Make sure password and password_confirm are the same.
+    chained_validators = [
+        formencode.validators.FieldsMatch('password', 'password_confirm')
+    ]
+
+class MaintainerForm(RegisterForm):
+    """
+    Schema for the maintainer registration form in the register controller.
+    """
+    email = NewEmailToSystem(not_empty=True)
+
+class SponsorForm(RegisterForm):
+    """
+    Schema for the sponsor registration form in the register controller.
+    """
+    email = NewDebianEmailToSystem(not_empty=True)
---
+++ debexpo/lib/validators.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+#
+#   validators.py — formencode validators for debexpo
+#
+#   This file is part of debexpo - http://debexpo.workaround.org
+#
+#   Copyright © 2008 Jonny Lamb <jonnylamb@jonnylamb.com
+#
+#   Permission is hereby granted, free of charge, to any person
+#   obtaining a copy of this software and associated documentation
+#   files (the "Software"), to deal in the Software without
+#   restriction, including without limitation the rights to use,
+#   copy, modify, merge, publish, distribute, sublicense, and/or sell
+#   copies of the Software, and to permit persons to whom the
+#   Software is furnished to do so, subject to the following
+#   conditions:
+#
+#   The above copyright notice and this permission notice shall be
+#   included in all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+#   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+#   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+#   OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+Holds the formencode validators for debexpo.
+"""
+
+__author__ = 'Jonny Lamb'
+__copyright__ = 'Copyright © 2008 Jonny Lamb'
+__license__ = 'MIT'
+
+import formencode
+import md5
+
+from debexpo.lib.base import *
+
+from debexpo.model import meta
+from debexpo.model.users import User
+
+class GpgKey(formencode.validators.FieldStorageUploadConverter):
+    """
+    Validator for an uploaded GPG key. They must with the 'BEGIN PGP PUBLIC KEY BLOCK'
+    text.
+    """
+
+    def _to_python(self, value, c):
+        """
+        Validate the GPG key.
+
+        ``value``
+            FieldStorage uploaded file.
+
+        ``c``
+        """
+        if not value.value.startswith('-----BEGIN PGP PUBLIC KEY BLOCK-----'):
+            raise formencode.Invalid(_('Invalid GPG key'), value, c)
+
+        return formencode.validators.FieldStorageUploadConverter._to_python(self, value, c)
+
+class CurrentPassword(formencode.validators.String):
+    """
+    Validator for a current password depending on the session's user_id.
+    """
+
+    def _to_python(self, value, c):
+        """
+        Validate the password.
+        """
+        user = meta.session.query(User).get(session['user_id'])
+
+        if user.password != md5.new(value).hexdigest():
+            raise formencode.Invalid(_('Incorrect password'), value, c)
+
+        return formencode.validators.String._to_python(self, value, c)
+
+class CheckBox(formencode.validators.Int):
+    """
+    Validator for a checkbox. When not checked, it doesn't send, and formencode
+    complains.
+    """
+    if_missing = None
+
+class NewEmailToSystem(formencode.validators.Email):
+    """
+    Email validator class to make sure there is not another user with
+    the same email address already registered.
+    """
+    allow = None
+
+    def _to_python(self, value, c):
+        """
+        Validate the email address.
+
+        ``value``
+            Address to validate.
+
+        ``c``
+        """
+        u = meta.session.query(User).filter_by(email=value)
+
+        # self.allow contains a user_id that should be ignored (i.e. when the user
+        # wants to keep the same email).
+        if self.allow is not None:
+            u = u.filter(User.id != self.allow)
+
+        u = u.first()
+
+        if u is not None:
+           raise formencode.Invalid(_('A user with this email address is already registered on the system'), value, c)
+
+        return formencode.validators.Email._to_python(self, value, c)
+
+class NewDebianEmailToSystem(NewEmailToSystem):
+    """
+    NewEmailToSystem validator class to make sure the user uses a @debian.org
+    email address.
+    """
+
+    def _to_python(self, value, c):
+        """
+        Validate the email address.
+
+        ``value``
+            Address to validate.
+
+        ``c``
+        """
+        if not value.endswith('@debian.org'):
+            raise formencode.Invalid(_('You must use your debian.org email address to register'), value, c)
+
+        return NewEmailToSystem._to_python(self, value, c)
