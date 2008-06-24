4100cc09cb2f2975d0728afd851afe0054062c89
Jonny Lamb <jonnylamb@jonnylamb.com>
2008-06-24 23:05:47 +0100

    Added beginning of plugins framework, starting with the addition of the post-upload hooks.
    Signed-off-by: Jonny Lamb <jonnylamb@jonnylamb.com>

---
+++ debexpo/lib/plugins.py
@@ -0,0 +1,158 @@
+# -*- coding: utf-8 -*-
+#
+#   plugins.py — Plugin loader
+#
+#   This file is part of debexpo - http://debexpo.workaround.org
+#
+#   Copyright © 2008 Jonny Lamb <jonnylamb@jonnylamb.com
+#
+#   Permission is hereby granted, free of charge, to any person
+#   obtaining a copy of this software and associated documentation
+#   files (the "Software"), to deal in the Software without
+#   restriction, including without limitation the rights to use,
+#   copy, modify, merge, publish, distribute, sublicense, and/or sell
+#   copies of the Software, and to permit persons to whom the
+#   Software is furnished to do so, subject to the following
+#   conditions:
+#
+#   The above copyright notice and this permission notice shall be
+#   included in all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+#   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+#   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+#   OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+Holds the plugin loader.
+"""
+
+__author__ = 'Jonny Lamb'
+__copyright__ = 'Copyright © 2008 Jonny Lamb'
+__license__ = 'MIT'
+
+import logging
+import os
+import tempfile
+import shutil
+
+from debexpo.lib.base import *
+
+log = logging.getLogger(__name__)
+
+# Different plugin stages and their options.
+plugin_stages = {
+    'post-upload' : {
+        'extract' : False,
+    }
+}
+
+class Plugins(object):
+
+    def __init__(self, type, changes, changes_file):
+        """
+        Class constructor. Sets class attributes and then runs the plugins.
+
+        ``type``
+            Type of plugins to run. (E.g. "post-upload")
+
+        ``changes``
+            Changes class for the package to test.
+
+        ``changes_file``
+            Name of the changes file.
+        """
+        self.type = type.replace('-', '_')
+        self.changes = changes
+        self.changes_file = changes_file
+        self.result = None
+        self.tempdir = None
+
+        # Run the plugins.
+        if type in plugin_stages:
+            self.conf = plugin_stages[type]
+            self.result = self._run_plugins()
+
+    def _import_plugin(self, name):
+        """
+        Imports a module and returns it.
+
+        This method was stolen from:
+            http://docs.python.org/lib/built-in-funcs.html
+
+        ``name``
+            Module to import.
+        """
+        try:
+            mod = __import__(name)
+            components = name.split('.')
+            for comp in components[1:]:
+                mod = getattr(mod, comp)
+            return mod
+        except ImportError:
+            return None
+
+    def _extract(self):
+        """
+        Copy the files to a temporary directory and run dpkg-source -x on the dsc file
+        to extract them.
+        """
+        self.tempdir = tempfile.mkdtemp()
+        for file in self.changes.get_files():
+            shutil.copy(os.path.join(config['debexpo.upload.incoming'], file), self.tempdir)
+        shutil.copy(os.path.join(config['debexpo.upload.incoming'], self.changes_file), self.tempdir)
+
+        self.oldcurdir = os.path.abspath(os.path.curdir)
+        os.chdir(self.tempdir)
+
+        os.system('dpkg-source -x %s extracted' % self.changes.get_dsc())
+
+    def _cleanup(self):
+        """
+        Remove the previously-created temporary directory and chdir back to where the importer
+        was.
+        """
+        if self.tempdir is not None:
+            shutil.rmtree(self.tempdir)
+            os.chdir(self.oldcurdir)
+
+    def _run_plugins(self):
+        """
+        Look in the config file and run the plugins.
+        """
+        plugins = config['debexpo.plugins.' + self.type].split(' ')
+        result = []
+
+        # Look at whether the plugins need extracting.
+        if self.conf['extract']:
+            self._extract()
+
+        # Run each plugin.
+        for plugin in plugins:
+            name = 'debexpo.plugins.%s.%s' % (self.type.replace('_', ''), plugin)
+            module = self._import_plugin(name)
+
+            if hasattr(module, 'plugin'):
+                p = getattr(module, 'plugin')(changes=self.changes, \
+                    changes_file=self.changes_file, tempdir=self.tempdir)
+
+                result.extend(p.run())
+
+        if self.conf['extract']:
+            self._cleanup()
+
+        return result
+
+    def stop(self):
+        """
+        Returns whether the importer should stop.
+        """
+        for result in self.result:
+            if result.stop():
+                return True
+
+        return False
---
+++ debexpo/plugins/__init__.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+#
+#   __init__.py — Helpful classes for plugins
+#
+#   This file is part of debexpo - http://debexpo.workaround.org
+#
+#   Copyright © 2008 Jonny Lamb <jonnylamb@jonnylamb.com
+#
+#   Permission is hereby granted, free of charge, to any person
+#   obtaining a copy of this software and associated documentation
+#   files (the "Software"), to deal in the Software without
+#   restriction, including without limitation the rights to use,
+#   copy, modify, merge, publish, distribute, sublicense, and/or sell
+#   copies of the Software, and to permit persons to whom the
+#   Software is furnished to do so, subject to the following
+#   conditions:
+#
+#   The above copyright notice and this permission notice shall be
+#   included in all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+#   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+#   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+#   OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+Holds some helpful classes for plugins to use or extend.
+"""
+
+__author__ = 'Jonny Lamb'
+__copyright__ = 'Copyright © 2008 Jonny Lamb'
+__license__ = 'MIT'
+
+from debexpo.lib import constants
+
+class BasePlugin(object):
+    """
+    The class all other plugins should extend.
+    """
+
+    def __init__(self, **kw):
+        """
+        Class constructor. Sets class attributes depending on the arguments of the
+        constructor.
+
+        ``kw``
+            Values to assign to class attributes.
+        """
+        for key in kw:
+            setattr(self, key, kw[key])
+
+    def run(self):
+        """
+        Runs all the tests in the self.tests list.
+        """
+        result = []
+        for test in self.tests:
+            if hasattr(self, test):
+                result.extend(getattr(self, test)())
+
+        return result
+
+    def passed(self, name, data, severity):
+        """
+        Returns a PluginResult for a passed test.
+
+        ``name``
+            Name of the plugin.
+
+        ``data``
+            Resulting data from the plugin, like more details about the process.
+
+        ``severity``
+            Severity of the result.
+        """
+        return PluginResult(from_plugin=name, outcome=constants.PLUGIN_OUTCOME_PASSED,
+            data=data, severity=severity)
+
+    def failed(self, name, data, severity):
+        """
+        Returns a PluginResult for a failed test.
+
+        ``name``
+            Name of the plugin.
+
+        ``data``
+            Resulting data from the plugin, like more details about the process.
+
+        ``severity``
+            Severity of the result.
+
+        """
+        return PluginResult(from_plugin=name, outcome=constants.PLUGIN_OUTCOME_FAILED,
+            data=data, severity=severity)
+
+class PluginResult(object):
+    """
+    The class tests should return to provide details about a test.
+    """
+
+    def __init__(self, from_plugin, outcome, data, severity):
+        """
+        Class constructor. Sets important fields.
+
+        ``from_plugin``
+            Name of the plugin the test was carried out in.
+
+        ``outcome``
+            Outcome of the test.
+
+        ``data``
+            More details of the test.
+
+        ``severity``
+            Severity of the result.
+        """
+        self.from_plugin = from_plugin
+        self.outcome = outcome
+        self.data = data
+        self.severity = severity
+
+    def failed(self):
+        """
+        Returns whether the test failed.
+        """
+        return self.outcome == constants.PLUGIN_OUTCOME_FAILED
+
+    def stop(self):
+        """
+        Returns whether the process should stop after the test.
+        """
+        return self.severity >= constants.PLUGIN_SEVERITY_ERROR
