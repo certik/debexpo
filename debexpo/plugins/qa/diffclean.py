2f786171f9ff5db1172a086d7bca3152488aa3f5
Jonny Lamb <jonnylamb@jonnylamb.com>
2008-06-25 15:26:24 +0100

    Added diffclean QA plugin.
    Signed-off-by: Jonny Lamb <jonnylamb@jonnylamb.com>

---
+++ debexpo/plugins/qa/diffclean.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+#
+#   diffclean.py — diffclean plugin
+#
+#   This file is part of debexpo - http://debexpo.workaround.org
+#
+#   Copyright © 2008 Jonny Lamb <jonnylamb@jonnylamb.com
+#
+#   Permission is hereby granted, free of charge, to any person
+#   obtaining a copy of this software and associated documentation
+#   files (the "Software"), to deal in the Software without
+#   restriction, including without limitation the rights to use,
+#   copy, modify, merge, publish, distribute, sublicense, and/or sell
+#   copies of the Software, and to permit persons to whom the
+#   Software is furnished to do so, subject to the following
+#   conditions:
+#
+#   The above copyright notice and this permission notice shall be
+#   included in all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+#   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+#   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+#   OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+Holds the diffclean plugin.
+"""
+
+__author__ = 'Jonny Lamb'
+__copyright__ = 'Copyright © 2008 Jonny Lamb'
+__license__ = 'MIT'
+
+import commands
+import logging
+
+from debexpo.lib import constants
+from debexpo.lib.base import *
+from debexpo.plugins import BasePlugin
+
+log = logging.getLogger(__name__)
+
+class DiffCleanPlugin(BasePlugin):
+    tests = ['test_diff_clean']
+
+    def test_diff_clean(self):
+        """
+        Check to make sure the diff.gz is clean.
+        """
+        log.debug('Checking to make sure the diff.gz is clean')
+
+        difffile = self.changes.get_diff()
+
+        if difffile is None:
+            log.warning('Package has no diff.gz file; native package?')
+            return
+
+        diffstat = commands.getoutput('diffstat -p1 -l %s' % difffile)
+
+        dirty = False
+        for item in diffstat.split('\n'):
+            if not item.startswith('debian/'):
+                dirty = True
+                break
+
+        if not dirty:
+            log.debug('Diff file %s is clean' % difffile)
+            self.passed(__name__, 'Diff file %s is clean' % difffile, constants.PLUGIN_SEVERITY_INFO)
+        else:
+            log.error('Diff file %s is not clean' % difffile)
+            self.failed(__name__, 'Diff file %s is not clean' % difffile, constants.PLUGIN_SEVERITY_ERROR)
+
+plugin = DiffCleanPlugin
