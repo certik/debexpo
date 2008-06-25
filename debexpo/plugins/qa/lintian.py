c6067f90a476ff017e6a60885ee90bbf6fa52ddd
Jonny Lamb <jonnylamb@jonnylamb.com>
2008-06-25 02:43:17 +0100

    Added lintian QA plugin.
    Signed-off-by: Jonny Lamb <jonnylamb@jonnylamb.com>

---
+++ debexpo/plugins/qa/lintian.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+#
+#   lintian.py — lintian plugin
+#
+#   This file is part of debexpo - http://debexpo.workaround.org
+#
+#   Copyright © 2008 Jonny Lamb <jonnylamb@jonnylamb.com
+#
+#   Permission is hereby granted, free of charge, to any person
+#   obtaining a copy of this software and associated documentation
+#   files (the "Software"), to deal in the Software without
+#   restriction, including without limitation the rights to use,
+#   copy, modify, merge, publish, distribute, sublicense, and/or sell
+#   copies of the Software, and to permit persons to whom the
+#   Software is furnished to do so, subject to the following
+#   conditions:
+#
+#   The above copyright notice and this permission notice shall be
+#   included in all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+#   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+#   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+#   OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+Holds the lintian plugin.
+"""
+
+__author__ = 'Jonny Lamb'
+__copyright__ = 'Copyright © 2008 Jonny Lamb'
+__license__ = 'MIT'
+
+import commands
+import logging
+import os
+
+from debexpo.lib import constants
+from debexpo.lib.base import *
+from debexpo.plugins import BasePlugin
+
+log = logging.getLogger(__name__)
+
+class LintianPlugin(BasePlugin):
+    tests = ['run_lintian']
+
+    def run_lintian(self):
+        """
+        Method to run lintian on the package.
+        """
+        log.debug('Running lintian on the package')
+
+        output = commands.getoutput('lintian %s' % self.changes_file)
+
+        items = output.split('\n')
+
+        if items and output != '':
+            severity = constants.PLUGIN_SEVERITY_WARNING
+            logmessage = log.warning
+            for item in items:
+                if item.startswith('E:'):
+                    severity = constants.PLUGIN_SEVERITY_ERROR
+                    logmessage = log.error
+                    break
+
+            output = 'Package is not Lintian clean\n' + output
+            logmessage('Package is not Lintian clean')
+            return [self.failed(__name__, output, severity)]
+        else:
+            log.debug('Package is Lintian clean')
+            return [self.passed(__name__, 'Package is Lintian clean', constants.PLUGIN_SEVERITY_INFO)]
+
+plugin = LintianPlugin
