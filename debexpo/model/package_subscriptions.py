e500f947d00915227623c2a7b16d1b1365806fbf
Jonny Lamb <jonnylamb@jonnylamb.com>
2008-08-07 22:24:54 +0100

    Added package_subscriptions table and model.
    Signed-off-by: Jonny Lamb <jonnylamb@jonnylamb.com>

---
+++ debexpo/model/package_subscriptions.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+#
+#   package_subscriptions.py — package_subscriptions table model
+#
+#   This file is part of debexpo - http://debexpo.workaround.org
+#
+#   Copyright © 2008 Jonny Lamb <jonnylamb@jonnylamb.com
+#
+#   Permission is hereby granted, free of charge, to any person
+#   obtaining a copy of this software and associated documentation
+#   files (the "Software"), to deal in the Software without
+#   restriction, including without limitation the rights to use,
+#   copy, modify, merge, publish, distribute, sublicense, and/or sell
+#   copies of the Software, and to permit persons to whom the
+#   Software is furnished to do so, subject to the following
+#   conditions:
+#
+#   The above copyright notice and this permission notice shall be
+#   included in all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+#   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+#   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+#   OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+Holds package_subscriptions table model.
+"""
+
+__author__ = 'Jonny Lamb'
+__copyright__ = 'Copyright © 2008 Jonny Lamb'
+__license__ = 'MIT'
+
+import sqlalchemy as sa
+from sqlalchemy import orm
+
+from debexpo.model import meta, OrmObject
+from debexpo.model.users import User
+
+t_package_subscriptions = sa.Table('package_subscriptions', meta.metadata,
+    sa.Column('id', sa.types.Integer, primary_key=True),
+    sa.Column('user_id', sa.types.Integer, sa.ForeignKey('users.id')),
+    sa.Column('package', sa.types.String(200), nullable=False),
+    sa.Column('level', sa.types.Integer, nullable=False),
+    )
+
+class PackageSubscription(OrmObject):
+    foreign = ['user']
+
+orm.mapper(PackageSubscription, t_package_subscriptions, properties={
+    'user' : orm.relation(User, backref='package_subscriptions')
+})
