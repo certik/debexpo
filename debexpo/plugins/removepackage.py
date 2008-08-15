3e99cabc61dc0fc80b19379ba7b9285f48f47d4b
Jonny Lamb <jonnylamb@jonnylamb.com>
2008-08-15 22:36:39 +0100

    Use cascading deletes to delete package(_version) dependant information instead of doing so explicitly.
    Signed-off-by: Jonny Lamb <jonnylamb@jonnylamb.com>

--- debexpo/plugins/removepackage.py
+++ debexpo/plugins/removepackage.py
@@ -55,43 +55,6 @@ log = logging.getLogger(__name__)
 
 class RemovePackagePlugin(BasePlugin):
 
-    def _remove_package_versions(self, package_versions):
-        """
-        Remove a package version and all its dependant parts.
-
-        ``package_versions``
-            List of PackageVersions to remove.
-        """
-        for package_version in package_versions:
-            source_binary = package_version.source_packages
-            source_binary.extend(package_version.binary_packages)
-
-            for source_package in source_binary:
-                for package_file in source_package.package_files:
-                    filename = os.path.join(config['debexpo.repository'], package_file.filename)
-                    if os.path.isfile(filename):
-                        log.debug('Removing package file: %s' % package_file.filename)
-                        os.remove(filename)
-                    else:
-                        log.warning('Could not find package file: %s' % package_file.filename)
-
-                    log.debug('Deleting package file database entry: %s' % package_file.filename)
-                    meta.session.delete(package_file)
-
-                log.debug('Deleting source package database entry')
-                meta.session.delete(source_package)
-
-            log.debug('Deleting package info')
-            for info in package_version.package_info:
-                meta.session.delete(info)
-
-            log.debug('Deleting package comments')
-            for comment in package_version.package_comments:
-                meta.session.delete(comment)
-
-            log.debug('Deleting package version: %s' % package_version.version)
-            meta.session.delete(package_version)
-
     def test_remove_package(self):
         """
         Removes a package that has been uploaded to Debian.
@@ -122,8 +85,9 @@ class RemovePackagePlugin(BasePlugin):
 
         else:
             # Only remove certain package versions.
-            to_delete = [x for x in package.package_versions if x not in keep_package_versions]
-            self._remove_package_versions(to_delete)
+            for package_version in package.package_versions:
+                if package_version not in keep_package_versions:
+                    meta.session.delete(package_version)
 
         meta.session.commit()
 
