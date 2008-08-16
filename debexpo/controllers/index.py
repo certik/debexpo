5862053e53ade05b107c7462b8334a02b94bfd35
Jonny Lamb <jonnylamb@jonnylamb.com>
2008-08-16 20:13:18 +0100

    Added a basic IndexController based on the current mentors.debian.net front page.
    Signed-off-by: Jonny Lamb <jonnylamb@jonnylamb.com>

---
+++ debexpo/controllers/index.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+#
+#   index.py — index controller
+#
+#   This file is part of debexpo - http://debexpo.workaround.org
+#
+#   Copyright © 2008 Jonny Lamb <jonnylamb@jonnylamb.com
+#
+#   Permission is hereby granted, free of charge, to any person
+#   obtaining a copy of this software and associated documentation
+#   files (the "Software"), to deal in the Software without
+#   restriction, including without limitation the rights to use,
+#   copy, modify, merge, publish, distribute, sublicense, and/or sell
+#   copies of the Software, and to permit persons to whom the
+#   Software is furnished to do so, subject to the following
+#   conditions:
+#
+#   The above copyright notice and this permission notice shall be
+#   included in all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+#   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+#   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+#   OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+Holds the IndexController.
+"""
+
+__author__ = 'Jonny Lamb'
+__copyright__ = 'Copyright © 2008 Jonny Lamb'
+__license__ = 'MIT'
+
+import logging
+
+from debexpo.lib.base import *
+from debexpo.controllers.packages import PackagesController
+
+log = logging.getLogger(__name__)
+
+class IndexController(BaseController):
+
+    def index(self):
+        pkg_controller = PackagesController()
+
+        c.config = config
+        c.packages = pkg_controller._get_packages()
+        return render('/index/index.mako')
